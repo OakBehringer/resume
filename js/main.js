260e14b779517281d112e6010305f58614e746af
adamkosecki <adam@latchel.com>
2025-04-14 10:43:15 -0400

    2025 version

--- js/main.js
+++
@@ -1,5 +0,0 @@
-$(document).on('ready', function () {
-	// Text fade in
-	$('header h1').akTextFadeIn();
-	$('.intro .me').akTextFadeIn();
-});
---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import akLogo from './assets/ak-logo.svg';
+
+function App() {
+
+  return (
+      <div className="w-md mx-auto text-neutral-700 text-center">
+          <img src={akLogo} alt="Adam Kosecki" />
+          <h1 className="text-xl font-black">Adam Kosecki</h1>
+
+          <div className="mt-12 text-lg space-y-4">
+              <div>
+                  <a className="underline" href="/docs/Adam%20Kosecki%20-%20Resume.pdf" target="_blank">Resume</a>
+              </div>
+              <div>
+                  <a className="underline" href="https://www.instagram.com/oakbehringer/" target="_blank">Instagram</a>
+              </div>
+              <div>
+                  <a className="underline" href="https://www.facebook.com/profile.php?id=856195000" target="_blank">Facebook</a>
+              </div>
+          </div>
+      </div>
+  )
+}
+
+export default App
